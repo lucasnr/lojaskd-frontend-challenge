acebdc6f0c4214b304c17eff3c752f4bbb5e9945
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-02-26 15:56:32 -0300

    Fixed label input association

--- src/pages/LoginPage/Input/index.js
+++ src/pages/LoginPage/Input/index.js
@@ -2,20 +2,21 @@ import React, { useState } from 'react';
 
 import { Container, Input, Label, Message } from './styles';
 
-export default function FormInput({ placeholder, ...rest }) {
+export default function FormInput({ placeholder, id, ...rest }) {
 	const [empty, setEmpty] = useState(true);
 	const [visited, setVisited] = useState(false);
 
 	return (
 		<Container empty={empty} visited={visited}>
 			<Input
+				id={id}
 				onBlur={event => {
 					setEmpty(event.target.value === '');
 					if (!visited) setVisited(true);
 				}}
 				{...rest}
 			/>
-			<Label>
+			<Label htmlFor={id}>
 				{placeholder} <span>*</span>
 			</Label>
 			<Message>Não pode ficar em branco.</Message>
